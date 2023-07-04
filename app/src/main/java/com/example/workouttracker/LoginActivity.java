8266aa33b9d0b8117ad5bcd06fb7a33f71ba8310
300360289 <tyh1004@gmail.com>
2023-07-04 14:09:49 -0700

    firebase

--- app/src/main/java/com/example/workouttracker/LoginActivity.java
+++ app/src/main/java/com/example/workouttracker/LoginActivity.java
@@ -8,15 +8,20 @@ import android.widget.Toast;
 
 import com.example.workouttracker.databinding.ActivityLoginPageBinding;
 import com.example.workouttracker.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginPageBinding binding;
+
+    FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityLoginPageBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
         setContentView(view);
+        firebaseAuth=FirebaseAuth.getInstance();
     }
     public void ClickLogin(View view) {
         Toast.makeText(this, "Login is clicked!", Toast.LENGTH_SHORT).show();
