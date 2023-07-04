2bc1f0b0b86f7fd309b5309caab00f9c9830b70b
300360289 <tyh1004@gmail.com>
2023-07-04 14:29:22 -0700

    firebase

--- app/src/main/java/com/example/workouttracker/RegisterActivity.java
+++ app/src/main/java/com/example/workouttracker/RegisterActivity.java
@@ -14,6 +14,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
