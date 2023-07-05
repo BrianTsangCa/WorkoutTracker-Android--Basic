65a584bc47f674bec172f6970dc865812ac07a4c
300360289 <tyh1004@gmail.com>
2023-07-05 16:21:35 -0700

    Change the main page to pending. move it to the work out now page.

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -20,39 +20,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-    UserDatabase udb;
-    private List<Workout> WorkOutList=new ArrayList<>();
-    //    private List<CalorieBurned> calorieBurned=new ArrayList<>();
-    RecyclerView workoutRecyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        workoutRecyclerview=findViewById(R.id.workout_recyclerview);
-        fetchWorkout();
-        udb= Room.databaseBuilder
-                (getApplicationContext(),UserDatabase.class,"user.db").build();
-        UserDao userDao=udb.userDao();
-
-
-        ExecutorService executorService= Executors.newSingleThreadExecutor();
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-
-                    }
-                });
-            }
-        });
-        workoutRecyclerview.setLayoutManager(new LinearLayoutManager(this));
-        workoutRecyclerview.setAdapter(new RecyclerAdapter(WorkOutList,this));
-
     }
-    private void fetchWorkout(){
-        WorkOutList.add(new Workout("Ski","Ski","300"));
-    }
+
 }
