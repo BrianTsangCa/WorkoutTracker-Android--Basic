3a7556a8ad7abb05817dcf3f85c08b4f78f30f30
300360289 <tyh1004@GMAIL.COM>
2023-07-11 10:45:52 -0700

    build of different fragments

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -13,6 +13,7 @@ import com.example.workouttracker.database.UserDatabase;
 import com.example.workouttracker.user.model.User;
 import com.example.workouttracker.user.model.UserDao;
 import com.example.workouttracker.workout.model.Workout;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +21,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationMenuView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+//        bottomNavigationView.setOnNavigationItemSelectedListener();
     }
 }
