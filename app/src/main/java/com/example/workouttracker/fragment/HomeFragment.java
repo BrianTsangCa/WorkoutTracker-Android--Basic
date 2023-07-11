c5203b0985f626625f466ad4bcd87db6f74bc4c4
300360289 <tyh1004@gmail.com>
2023-07-11 16:46:55 -0700

    add a ","

--- app/src/main/java/com/example/workouttracker/fragment/HomeFragment.java
+++ app/src/main/java/com/example/workouttracker/fragment/HomeFragment.java
@@ -93,7 +93,7 @@ public class HomeFragment extends Fragment {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        home_intro.setText("Welcome to workout tracker "+currentUser.getUserName()+" ! ");
+                        home_intro.setText("Welcome to workout tracker, "+currentUser.getUserName()+" ! ");
                     }
                 });
             }
