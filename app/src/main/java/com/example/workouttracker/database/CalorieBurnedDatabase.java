45a85856ef1de64a57baece84ca1f31c5b7d688b
300360289 <tyh1004@gmail.com>
2023-07-11 16:48:56 -0700

    another database

---
+++ app/src/main/java/com/example/workouttracker/database/CalorieBurnedDatabase.java
@@ -0,0 +1,14 @@
+package com.example.workouttracker.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.workouttracker.CalorieBurned.model.CalorieBurned;
+import com.example.workouttracker.CalorieBurned.model.CalorieBurnedDao;
+import com.example.workouttracker.user.model.User;
+import com.example.workouttracker.user.model.UserDao;
+
+@Database(entities = {CalorieBurned.class}, version=1, exportSchema = false)
+public abstract class CalorieBurnedDatabase extends RoomDatabase {
+    public abstract CalorieBurnedDao calorieBurnedDao();
+}
