a893888b48fd2787f592ebf70f82fe8c034164e8
300360289 <tyh1004@gmail.com>
2023-07-12 10:23:11 -0700

    data of CalorieBurned saved after stop.

--- app/src/main/java/com/example/workouttracker/WorkoutCountingActivity.java
+++ app/src/main/java/com/example/workouttracker/WorkoutCountingActivity.java
@@ -1,6 +1,7 @@
 package com.example.workouttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.workouttracker.CalorieBurned.model.CalorieBurned;
+import com.example.workouttracker.CalorieBurned.model.CalorieBurnedDao;
+import com.example.workouttracker.LoginAndRegisterAcitivties.RegisterPageActivity;
+import com.example.workouttracker.database.CalorieBurnedDatabase;
+import com.example.workouttracker.database.UserDatabase;
+import com.example.workouttracker.user.model.User;
+import com.example.workouttracker.user.model.UserDao;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WorkoutCountingActivity extends AppCompatActivity {
     TextView txtView_name,txtView_time;
@@ -20,6 +36,10 @@ public class WorkoutCountingActivity extends AppCompatActivity {
     Runnable runnable;
     Handler handler=new Handler(Looper.myLooper());
 
+    CalorieBurnedDao calorieBurnedDao;
+    CalorieBurnedDatabase calorieBurnedDatabase;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +48,17 @@ public class WorkoutCountingActivity extends AppCompatActivity {
         txtView_name=findViewById(R.id.txtView_name);
         txtView_time=findViewById(R.id.txtView_time);
         btn_start_stop=findViewById(R.id.btn_start_stop);
+        calorieBurnedDatabase = Room.databaseBuilder
+                (getApplicationContext(), CalorieBurnedDatabase.class,"calorieBurned.db").build();
+        calorieBurnedDao = calorieBurnedDatabase.calorieBurnedDao();
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
 
-//        Bundle bundle=getIntent().getExtras();
-//        String name=bundle.getString("name");
-//        int calories_per_hour=bundle.getInt("calories_per_hour");
-//        txtView_name.setText(name);
+        Bundle bundle=getIntent().getExtras();
+        String name=bundle.getString("name");
+        int calories_per_hour=bundle.getInt("calories_per_hour");
+        txtView_name.setText(name);
 
         String time=String.format("%02d:%02d:%02d", second/60/60, second/60, second%60);
         txtView_time.setText(time);
@@ -61,11 +87,31 @@ public class WorkoutCountingActivity extends AppCompatActivity {
                     btn_start_stop.setImageResource(R.drawable.start);
                     isCounting=false;
                     handler.removeCallbacks(runnable);
-                    second = 0;
+
                     String time=String.format("%02d:%02d:%02d", second/60/60, second/60, second%60);
                     txtView_time.setText(time);
-                    Intent intent=new Intent();
+                    Intent intent=new Intent(WorkoutCountingActivity.this,MainActivity.class);
+                    Calendar calendar = Calendar.getInstance();
+                    int year = calendar.get(Calendar.YEAR);
+                    int month = calendar.get(Calendar.MONTH) + 1;
+                    int day = calendar.get(Calendar.DAY_OF_MONTH);
+                    CalorieBurned calorieBurnedData = new CalorieBurned(email,year,month,day,calories_per_hour*second/60/60);
+                    ExecutorService executorService= Executors.newSingleThreadExecutor();
+                    executorService.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            calorieBurnedDao.insertCalorieBurned(calorieBurnedData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
 
+                                }
+                            });
+                        }
+                    });
+                    second = 0;
+                    Toast.makeText(WorkoutCountingActivity.this, "Calorie Burned Data -("+calorieBurnedData.getTotalCalorieBurned()+" ) is Created!", Toast.LENGTH_LONG).show();
+                    startActivity(intent);
                 }
             }
         });
